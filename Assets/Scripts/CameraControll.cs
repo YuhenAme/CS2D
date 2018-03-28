edd5626e99809fb8bd723473269eed98b31f3642
Sakuralalala <1209251178@qq.com>
2018-03-28 12:26:03 +0800

    修改
    修改...以及添加地图

---
+++ Assets/Scripts/CameraControll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour {
+
+    /// <summary>
+    /// 相机要跟随的物体
+    /// </summary>
+    [SerializeField]
+    private GameObject player;
+    /// <summary>
+    /// 相机的刚体控制相机运动
+    /// </summary>
+    private Rigidbody2D mainRigid;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+        mainRigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.LookAt(playerTransform);
+        //transform.Translate(new Vector3(playerPos.position.x, playerPos.position.y, -10));
+        mainRigid.velocity = player.GetComponent<Rigidbody2D>().velocity;
+	}
+}
